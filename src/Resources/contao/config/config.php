8265c66dd9807ebb7cfce1a8d7a3dd20820481a5
Christian Romeni <christianromeni@users.noreply.github.com>
2023-03-09 21:48:48 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -15,13 +15,13 @@ use Rwd\ContaoCustomArticlesBundle\Elements\NewRow;
 /*
  * Content elements
  */
-// array_push($GLOBALS['TL_CTE']['style'], count($GLOBALS['TL_CTE']['slider']), [
-//     'newRow' => NewRow::class,
-// ]);
+Contao\ArrayUtil::arrayInsert($GLOBALS['TL_CTE']['style'], count($GLOBALS['TL_CTE']['slider']), [
+    'newRow' => NewRow::class,
+]);
 
 /*
  * Wrapper elements
  */
-// array_push($GLOBALS['TL_WRAPPERS']['separator'], count($GLOBALS['TL_WRAPPERS']['separator']), [
-//     'NewRow',
-// ]);
+Contao\ArrayUtil::arrayInsert($GLOBALS['TL_WRAPPERS']['separator'], count($GLOBALS['TL_WRAPPERS']['separator']), [
+    'NewRow',
+]);
